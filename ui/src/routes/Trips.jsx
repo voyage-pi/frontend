78945aab0ce8cfafc3a9323d06a33ac52010a9f6
ruimachado23 <rui.faria.machado@gmail.com>
2025-03-16 18:34:21 +0000

    trips-page(trip-card): accurate info

--- ui/src/routes/Trips.jsx
+++ ui/src/routes/Trips.jsx
@@ -4,31 +4,30 @@ import TripCard from "../components/TripCard"
 function Trips() {
   return (
     <PageTemplate>
-      <h1 className="text-3xl font-bold mb-4">My Trips</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <TripCard
           image="https://i.pinimg.com/736x/1e/69/d6/1e69d69083d98c4ac2b37fcd3a21c978.jpg"
-          days={7}
+          days={4}
           people={2}
           destinations={3}
           name="Trip to Bali"
-          date="June 2022"
+          date="18 May 2025 - 23 May 2025"
         />
         <TripCard
           image="https://i.pinimg.com/736x/6b/f7/56/6bf756672824e2f961d661809649f0b7.jpg"
-          days={10}
+          days={8}
           people={4}
           destinations={5}
-          name="Trip to Japan"
-          date="July 2022"
+          name="Trip to Cyprus"
+          date="10 Aug 2025 - 14 Aug 2025"
         />
         <TripCard
           image="https://i.pinimg.com/736x/97/70/be/9770bee8dae261fbf16eaf952aa1e409.jpg"
-          days={14}
+          days={10}
           people={6}
           destinations={7}
-          name="Trip to Europe"
-          date="August 2022"
+          name="Trip to Barcelona"
+          date="18 Oct 2025 - 28 Oct 2025"
         />
       </div>
 
