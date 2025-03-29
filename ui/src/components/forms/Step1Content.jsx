075987da53dc691fe1d6fae89a971a0effafe5e7
ruimachado23 <rui.faria.machado@gmail.com>
2025-03-29 16:06:05 +0000

    step1(infos): add texts

--- ui/src/components/forms/Step1Content.jsx
+++ ui/src/components/forms/Step1Content.jsx
@@ -15,13 +15,13 @@ const Step1Content = () => {
       id: 'individual',
       icon: FaUser,
       title: 'Individual Trip',
-      text: "blablabla1",
+      text: "Are you a solo traveler looking for a personal experiences? This option allows you to customize your itinerary based on your specific preferences, schedule, and interests without needing to coordinate with others. Perfect for self-discovery, personal adventures, or business travelers seeking convenience.",
     },
     {
       id: 'group',
       icon: FaUserGroup,
       title: 'Group Trip',
-      text: "blablabla2",
+      text: "Ideal for traveling with friends, family, or colleagues. You'll be able to customize your itinerary to accommodate everyone's interests and needs, creating a collaborative travel experience that strengthens bonds and creates lasting memories together.",
     },
   ];
 
