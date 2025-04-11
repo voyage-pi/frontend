c878214efd9cbe70a330e9f20be7f226fd672bfd
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-04-11 17:55:57 +0100

    Added suggestion and interaction with the map

--- ui/src/components/forms/step3/ZoneTripContent.jsx
+++ ui/src/components/forms/step3/ZoneTripContent.jsx
@@ -1,121 +1,240 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { axiosPlace } from '../../../utils/axiosInstance';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { FaSistrix } from 'react-icons/fa6';
 import RangeSlider from '../../RangeSlider';
 import Map from "../../Map";
+import { ToastContainer } from "react-toastify";
+import Notification from "../../Notification";
+import LoadingAnimation from "../../LoadingAnimation";
 
 const ZoneTripContent = () => {
-    const [selectedLocation, setSelectedLocation] = useState('Barcelona');
 
-    const handleSelectLocation = (location) => {
-        setSelectedLocation(location);
-        localStorage.setItem('Location', location);
-    };
+  const [circle, setCircle] = useState([])
+  const [radius, setRadius] = useState(100)
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [suggestionlist, setSuggestionList] = useState([]);
+  const [currentText, setCurrentText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [suggestionHovered, setSuggestionHovered] = useState(-1);
+  const [notify, setNotify] = useState()
+  const [markers, setMarkers] = useState([]);
+  const timeoutRef = useRef(null);
+  const handleSelectLocation = async (location) => {
+    setSelectedLocation(location);
+    setCurrentText(location);
 
-    useEffect(() => {
-        const savedLocation = localStorage.getItem('Location');
-        if (savedLocation) {
-            setSelectedLocation(savedLocation);
-        }
-    }, []);
-
-    const locations = [
-        { id: 1, name: 'Barcelona' },
-        { id: 2, name: 'Barceloneta Beach Espanha' },
-        { id: 3, name: 'Bacelona led 1' },
-        { id: 4, name: 'Confeitaria Barcelona By Ayete' }
-    ];
-    const [budget, setBudget] = useState(332);
-
-    const handleBudgetChange = (newBudget) => {
-        setBudget(newBudget);
-        localStorage.setItem('Budget', newBudget);
-    };
-
-    const myPolylines = [
-        {
-          polylines: [
-            {
-                polylineEncoded: "",
-                duration: 0,
-                distance: 0
-            },
-          ],
+    try {
+      const response = await axiosPlace.post("/search/", {
+        place_name: location,
+      });
+      let m = {
+        position: {
+          lat: response.data.latitude,
+          lng: response.data.longitude,
         },
-      ];
-    
-      const myMarkers = [
-        {
-            position: { lat: 32.61402777012159, lng: -8.656425489382625 },
-            title: "DETI",
-            address: "Universidade de Aveiro, 3810-193 Aveiro",
-            image: "https://lh3.googleusercontent.com/p/AF1QipNIoDTmCa7-LUb4p804W_pnaVl6vJOBrl7yFo7H=w408-h255-k-no",
-        },
-        {
-            position: { lat: 40.637322817325355, lng: -8.650697327204432 },
-            title: "Santos da Praça",
-            address: "Largo da Praça do Peixe 3, 3800-241 Aveiro",
-            image: "https://lh3.googleusercontent.com/p/AF1QipM5l6T80v1PyOOVb7PTDCOdp-oiF0BSwNnypcg=w426-h240-k-no",
+        title: location,
+        address: "",
+        image: "",
+      };
+
+      setCircle([{
+        "radius": radius,
+        "center": {
+          "lat": response.data.latitude,
+          "lng": response.data.longitude
         }
-      ];
+      }])
 
-    return (
-        <div>
-            <div className="flex flex-col md:flex-row gap-6">
-                {/* Left Side - Location Selection */}
-                <div className="flex-1">
-                    <h2 className="text-3xl mb-10 text-center">
-                        Choose an <span className="text-primary">area</span> to go to!
-                    </h2>
-                    {/* Search Box */}
-                    <div className="relative mb-4">
-                        <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-                            <FaSistrix className="text-gray-400 text-xl" />
-                        </div>
-                        <input
-                            type="text"
-                            className="pl-10 p-3 w-full border border-gray-200 rounded-lg focus:outline-none"
-                            placeholder="Barcelona"
-                        />
-                    </div>
+      localStorage.setItem("Longitude", response.data.longitude);
+      localStorage.setItem("Latitude", response.data.latitude);
+      setMarkers([m]);
 
-                    <RangeSlider
-                        value={budget}
-                        onChange={handleBudgetChange}
-                        min={0}
-                        max={2500}
-                        step={1}
-                        currency="€"
-                        rangeClassName="range range-primary range-sm"
-                        valueClassName="text-primary text-5xl font-bold mb-6"
-                        variant='compact'
-                    />
+    } catch (error) {
+      setNotify({
+        type: 'error',
+        text: `There was an error ${error}`,
+        key: Date.now()
+      })
+      console.error("Search error:", error);
+    }
+    localStorage.setItem("Location", location);
+  };
+
+  const handleRadiusChange = (radius) => {
+    setRadius(radius);
+    if (circle.length !== 0) {
+      setCircle([{ ...circle[0], "radius": radius }])
+    }
+
+    localStorage.setItem('Budget', radius);
+  };
+
+  useEffect(() => {
+    const savedLocation = localStorage.getItem('Location');
+    if (savedLocation) {
+      setSelectedLocation(savedLocation);
+    }
+  }, []);
 
 
-                    <div className="space-y-3">
-                        {locations.map(location => (
-                            <div
-                                key={location.id}
-                                className={`flex items-center p-3 rounded-lg text-lg cursor-pointer ${location.name === selectedLocation ? 'bg-primary text-white' : 'bg-gray-50'
-                                    }`}
-                                onClick={() => handleSelectLocation(location.name)}
-                            >
-                                <FaMapMarkerAlt className={`mr-3 ${location.name === selectedLocation ? 'text-white' : 'text-primary'}`} />
-                                <span>{location.name}</span>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+  const autocompleteSearch = async () => {
+    try {
+      const response = await axiosPlace.post("/places/autocomplete", {
+        input: currentText,
+      });
+      setSuggestionList(response.data.suggestions_list);
+      setLoading(false);
+    } catch (error) {
+      setNotify({
+        type: 'error',
+        text: `There was an error ${error}`,
+        key: Date.now()
+      })
+      console.error("Search error:", error);
+    }
+  };
 
-                <div className="flex-1 bg-blue-50 rounded-lg overflow-hidden h-109 flex-col -mb-10">
-                    <Map
-                        polylines={myPolylines}
-                        markers={myMarkers}
-                        />
-                </div>
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setLoading(value !== "");
+    setCurrentText(value);
+    if (value == "") {
+      return 0
+    }
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (value.length > 2) {
+      timeoutRef.current = setTimeout(() => {
+        autocompleteSearch();
+      }, 1200);
+    }
+  };
+
+  const handleSuggestionsSelection = (event) => {
+    if (suggestionlist.length === 0)
+      return
+    console.log(event)
+    let key = event.key
+    let suggestionsL = suggestionlist.length != 0 ? suggestionlist.length : 1
+    if (key === "ArrowDown") {
+      setSuggestionHovered(prev => (prev + 1) % suggestionsL)
+    }
+    else if (key === "ArrowUp") {
+      setSuggestionHovered(prev => ((prev <= 0 ? suggestionsL : prev) - 1) % suggestionsL)
+    }
+    else if (key === "Enter") {
+      let currentSelectedSuggestion = suggestionlist[suggestionHovered]
+      console.log(currentSelectedSuggestion)
+      handleSelectLocation(currentSelectedSuggestion.text)
+    }
+  }
+
+  const MouseHover = (idx) => {
+    setSuggestionHovered(idx)
+  }
+
+
+  return (
+    <div>
+
+      <ToastContainer />
+
+      {notify && (
+        <Notification
+          key={notify.key}
+          type={notify.type}
+          text={notify.text}
+          onClose={() => setNotify(null)}
+          options={{
+            position: "top-right",
+            autoClose: 3000,
+            pauseOnHover: false
+          }}
+        />
+      )}
+      <div className="flex flex-col md:flex-row gap-6">
+        {/* Left Side - Location Selection */}
+        <div className="flex-1">
+          <h2 className="text-3xl mb-10 text-center">
+            Choose a <span className="text-primary">location</span> to go to!
+          </h2>
+          {/* Search Box */}
+          <div className="relative mb-4">
+            <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
+              <FaSistrix className="text-gray-400 text-xl" />
             </div>
+            <input
+              onKeyDown={handleSuggestionsSelection}
+              onChange={handleInputChange}
+              value={currentText}
+              type="text"
+              className="pl-10 p-3 w-full border border-gray-200 rounded-lg focus:outline-none"
+              placeholder="Barcelona"
+            />
+          </div>
+
+          <div className="space-y-3 text-center">
+            {currentText.length > 3 ? (
+              loading ? (
+                <div className=" flex justify-center">
+                  <LoadingAnimation width={"150px"} height={"150px"} />
+                </div>
+              ) : (
+                <>
+                  <RangeSlider
+                    value={radius}
+                    onChange={handleRadiusChange}
+                    min={0}
+                    max={10000}
+                    step={100}
+                    currency="m"
+                    rangeClassName="range range-primary range-sm"
+                    valueClassName="text-primary text-5xl font-bold mb-6"
+                    variant='compact'
+                  />
+                  {suggestionlist.map((location, idx) => (
+                    <div
+                      onMouseEnter={() => MouseHover(idx)}
+                      onMouseLeave={() => setSuggestionHovered(-1)}
+                      key={location.place_id}
+                      className={`transition-all ease-in-out flex items-center p-3 rounded-lg text-lg cursor-pointer ${location.text === selectedLocation
+                        ? "bg-primary text-white"
+                        : "bg-gray-50"
+                        }
+                     ${suggestionHovered === idx && selectedLocation != location ? "translate-x-2 border-primary border-1" : ""} 
+                      
+`}
+                      onClick={() => handleSelectLocation(location.text)}
+                    >
+                      <FaMapMarkerAlt
+                        className={`mr-3 ${location.text === selectedLocation
+                          ? "text-white"
+                          : "text-primary"
+                          }`}
+                      />
+                      <span>{location.text}</span>
+                    </div>
+                  ))}
+                </>
+              )
+            ) : (
+
+              <div className="w-full text-primary opacity-50 text-center my-3">
+                Insert a location that you would like to visit...
+              </div>
+            )}
+          </div>
         </div>
-    );
+
+        <div className="flex-1 bg-blue-50 rounded-lg overflow-hidden h-109 flex-col -mb-10">
+          <Map markers={markers} circles={circle} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ZoneTripContent;
