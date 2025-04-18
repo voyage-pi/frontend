f5589ec88daf8a9717209beec96de3f7e7d13b64
guilherme096 <guilherme.rosa60@gmail.com>
2025-04-18 20:12:16 +0100

    enable activity regeneration

--- ui/src/utils/axiosInstance.js
+++ ui/src/utils/axiosInstance.js
@@ -12,15 +12,22 @@ export const axiosPlace = axios.create({
   baseURL: "/api/v1/place-wrapper", // Updated to use the nginx proxy path
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export const axiosUser = axios.create({
-  baseURL: "/api/v1/user-management", 
+  baseURL: "/api/v1/user-management",
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
+export const axiosRecommendation = axios.create({
+  baseURL: "/api/v1/recommendations",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
