df82f87e8fe63b5edc885d16ed23ed875556126b
ruimachado23 <rui.faria.machado@gmail.com>
2025-04-19 13:22:57 +0100

    page(friends): change notification button

--- ui/src/routes/Friends.jsx
+++ ui/src/routes/Friends.jsx
@@ -98,10 +98,11 @@ function Friends() {
               </div>
               <div className="flex items-center">
                 <button 
-                  className="p-2 relative bg-gray-100 rounded-full hover:bg-gray-200"
+                  className="p-2 px-4 relative bg-gray-100 rounded-full hover:bg-gray-200 flex items-center"
                   onClick={handleShowInvites}
                 >
-                  <FaEnvelope className="text-gray-600" />
+                  <FaEnvelope className="text-gray-600 mr-2" />
+                  <span className="text-gray-600 font-medium">Inbox</span>
                   {requestCount > 0 && (
                     <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
                       {requestCount}
