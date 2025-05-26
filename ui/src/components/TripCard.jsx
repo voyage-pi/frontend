6b9c44bf1bb80a58b1ad54986d5ff13fc28a5924
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2025-05-26 17:55:30 +0100

    Add trip type to TripCard and Trips components for enhanced itinerary details

--- ui/src/components/TripCard.jsx
+++ ui/src/components/TripCard.jsx
@@ -8,6 +8,7 @@ function TripCard({
   image,
   name, 
   date, 
+  type,
   days, 
   people, 
   destinations, 
@@ -147,7 +148,7 @@ function TripCard({
             <div className="flex justify-between items-center">
               <div>
                 <h2 className="text-xl font-bold mb-1">{name}</h2>
-                {!isSavedPlace && date && <p className="text-secondary/70">{date}</p>}
+                {!isSavedPlace && date && type!=="road" && <p className="text-secondary/70">{date}</p>}
                 {isSavedPlace && location && <p className="text-secondary/70">{location}</p>}
               </div>
             </div>
